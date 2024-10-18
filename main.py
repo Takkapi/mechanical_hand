6efb93ce390572cf857501860ebaa5ab9a42af98
takkapi <takkapi.off@gmail.com>
2024-10-18 17:20:52 +0300

    Added the hand function and fixed the movement of the arm on the X axis

--- main.py
+++ main.py
@@ -7,14 +7,86 @@ from pybricks.tools import wait, StopWatch, DataLog
 from pybricks.robotics import DriveBase
 from pybricks.media.ev3dev import SoundFile, ImageFile
 
-
-# This program requires LEGO EV3 MicroPython v2.0 or higher.
-# Click "Open user guide" on the EV3 extension tab for more information.
-
-
-# Create your objects here.
 ev3 = EV3Brick()
+# buttons = Button()
+motorX = Motor(Port.C)
+hand = Motor(Port.A)
+handClosed = False
+
+# Set hand to the default position
+# if hand.angle() < 55:
+#     hand.run_target(500, 50, then=Stop.HOLD)
+# else: pass
 
 
-# Write your program here.
-ev3.speaker.beep()
+
+sensor = TouchSensor(Port.S1)
+
+while(True):
+    pressed = ev3.buttons.pressed()
+    ev3.light.on(Color.ORANGE)
+    # motorX.stop()
+
+    # Main hand
+    if(Button.CENTER in pressed):
+        if handClosed == False:
+            ev3.light.on(Color.GREEN)
+            hand.run_target(400, -50, then=Stop.HOLD)
+            if hand.control.stalled():
+                pass
+            handClosed = True
+            print(hand.angle())
+            pass
+            # break
+        elif handClosed == True:
+            ev3.light.on(Color.GREEN)
+            hand.run_target(400, 50, then=Stop.HOLD)
+            if hand.control.stalled():
+                pass
+            handClosed = False
+            print(hand.angle())
+            pass
+            # break
+
+    if(Button.LEFT in pressed):
+        ev3.light.on(Color.GREEN)
+        motorX.run(-120)
+        # motorX.dc(20)
+        pass
+    elif True:
+        pass
+    else: motorX.stop() pass
+
+
+    if(Button.RIGHT in pressed):
+        ev3.light.on(Color.GREEN)
+        motorX.run(120)
+        # motorX.dc(-20)
+        pass
+    elif Button.RIGHT in pressed and sensor.pressed():
+        ev3.speaker.beep(500, 100)
+        ev3.light.on(Color.RED)
+        pass
+    else: motorX.stop() pass
+
+    # if Button.RIGHT in pressed and sensor.pressed():
+    #     ev3.speaker.beep(500, 100)
+    #     ev3.light.on(Color.RED)
+    #     pass
+
+
+    # if Button.LEFT in pressed:
+        # elif Button.RIGHT in pressed:
+
+
+# motorX.run(-90)
+
+# wait(5000)
+
+ev3.light.on(Color.RED)
+# motorX.stop()
+
+
+# if ev3.button.pressed(button.left):
+#     pass
+
